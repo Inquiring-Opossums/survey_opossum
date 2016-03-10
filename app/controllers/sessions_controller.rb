fb2e6eeb983b4950ebc6e411c3f62d3380288fb7
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-03-10 17:14:33 -0500

    Create sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = Author.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+
+      redirect_to user_dashboard_path, notice: "You have successfully logged in!"
+      # redirect_to send("#{params[:user_type].downcase}s_path"), notice: "You have successfully logged in!"
+    else
+      flash.now[:alert] = "Login failed: invalid email or password."
+      render "new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to login_path, notice: "You have logged out."
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ActiveRecord::Base
   has_many :surveys
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
