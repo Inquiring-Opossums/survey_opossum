c9761dcc16bb618fb62194b9da39ab4c08fd2bc8
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-03-12 09:17:55 -0500

    Define set_user method in survey controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,6 +1,6 @@
 class SurveysController < ApplicationController
   before_action :set_survey, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   # GET /surveys
   # GET /surveys.json
   def index
@@ -15,7 +15,7 @@ class SurveysController < ApplicationController
   # GET /surveys/new
   def new
     @survey = Survey.new
-    @user = Author.find_by_id(session[:user_id])
+    @user = Author.find_by_name(params[:name])
   end
 
   # GET /surveys/1/edit
@@ -48,13 +48,17 @@ class SurveysController < ApplicationController
   # DELETE /surveys/1.json
   def destroy
     @survey.destroy
-    redirect_to surveys_url, notice: 'Survey was successfully destroyed.' 
+    redirect_to surveys_url, notice: 'Survey was successfully destroyed.'
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_survey
       @survey = Survey.find(params[:id])
+    end
+
+    def set_user
+      @user = Author.find(session[:user_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
