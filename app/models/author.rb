2c42464d36434b3fdf66f812a86e07949ae4da2e
damianhouse <damianhouse@gmail.com>
2016-03-13 12:58:04 -0400

    Add all user questions to side of new surveys.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ActiveRecord::Base
   has_many :surveys
+  has_many :questions, through: :surveys
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
