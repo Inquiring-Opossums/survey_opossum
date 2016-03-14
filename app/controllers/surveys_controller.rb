073c7cb58f0a664e534b3212cea4c4309d3b5ecf
mkg1 <mkgehring@Mollys-MacBook-Pro.local>
2016-03-14 11:56:35 -0400

    Make surveys takable and assign taker id to surveys taken

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -67,10 +67,9 @@ class SurveysController < ApplicationController
   end
 
   def take
+    @taker = Taker.create
     @survey = Survey.find(params[:survey_id])
-    @survey.questions.build
-    @questions = @survey.questions.all.to_a.find(params[:question_id])
-    @questions.to_a.each do |q|
+    @survey.questions.each do |q|
       q.answers.build
     end
   end
@@ -86,6 +85,8 @@ class SurveysController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def survey_params
-      params.require(:survey).permit(:author_id, :name, :description, :categories, :published, questions_attributes: [:id, :question_text, :description, :taker_input, :_destroy, answers_attributes: [:id, :question_id, :taker_id, :answer]])
+      params.require(:survey).permit(:author_id, :name, :description, :categories, :published,
+      questions_attributes: [:id, :question_text, :description, :taker_input, :_destroy,
+      answers_attributes: [:id, :question_id, :taker_id, :answer]])
     end
 end
